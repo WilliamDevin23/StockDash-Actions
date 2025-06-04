df2287c8cc2f869d417450b4d6ed47063000acb4
William Devin S.P. <145544434+WilliamDevin23@users.noreply.github.com>
2025-06-04 18:28:57 +0700

    Add return statement to each endpoint

--- index.py
+++ index.py
@@ -10,28 +10,31 @@ def index() :
     try :
         return "Halo!"
     except Exception as e:
-        print(f"Error happened : {e}")
+        return f"Error happened : {e}"
 
 @app.route("/update_price")
 def api_update_price() :
     try :
         update_price()
+        return "Successfully update price"
     except Exception as e:
-        print(f"Error happened : {e}")
+        return f"Error happened : {e}"
 
 @app.route("/update_news")
 def api_update_news() :
     try :
         update_news()
+        return "Successfully update news"
     except Exception as e:
-        print(f"Error happened : {e}")
+        return f"Error happened : {e}"
         
 @app.route("/update_volume")
 def api_update_volume() :
     try :
         update_volume()
+        return "Successfully update trade volume"
     except Exception as e:
-        print(f"Error happened : {e}")
+        return f"Error happened : {e}"
         
 if __name__ == "__main__" :
     app.run()
